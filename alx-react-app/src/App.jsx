a09b9c2e20129ec1f14e76c1dd0b9ff0afa844b3
Timztech <adetunjitimothy7@gmail.com>
2025-02-16 23:21:49 +0100

    second assignment2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +32,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+       <MainContent /> 
+       <Footer />
     </>
   )
 }
