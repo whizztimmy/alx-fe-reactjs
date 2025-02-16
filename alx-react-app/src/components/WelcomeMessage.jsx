d1c394d21f2fd4864299330992b6567b6c3d9245
Timztech <adetunjitimothy7@gmail.com>
2025-02-16 23:02:33 +0100

    first assignment

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from './WelcomeMessage'
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+    <WelcomeMessage />
+}
+
+export default WelcomeMessage;
