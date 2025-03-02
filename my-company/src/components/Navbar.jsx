d46b69e8b195e3221711d4d2dd5c4120c0cf3cdb
Timztech <adetunjitimothy7@gmail.com>
2025-03-02 17:58:10 +0100

    Fixed Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: "#333", padding: "10px", textAlign: "center" }}>
-      <Link to="/" style={{ color: "white", margin: "10px" }}>Home</Link>
-      <Link to="/about" style={{ color: "white", margin: "10px" }}>About</Link>
-      <Link to="/services" style={{ color: "white", margin: "10px" }}>Services</Link>
-      <Link to="/contact" style={{ color: "white", margin: "10px" }}>Contact</Link>
+    <nav style={{ 
+      display: "flex", 
+      justifyContent: "space-between", 
+      alignItems: "center", 
+      backgroundColor: "navy", 
+      padding: "10px" 
+    }}>
+      <h2 style={{ color: "white" }}>My Company</h2>
+      <div>
+        <Link to="/" style={{ color: "white", margin: "0 10px", textDecoration: "none" }}>Home</Link>
+        <Link to="/about" style={{ color: "white", margin: "0 10px", textDecoration: "none" }}>About</Link>
+        <Link to="/services" style={{ color: "white", margin: "0 10px", textDecoration: "none" }}>Services</Link>
+        <Link to="/contact" style={{ color: "white", margin: "0 10px", textDecoration: "none" }}>Contact</Link>
+      </div>
     </nav>
   );
 }
