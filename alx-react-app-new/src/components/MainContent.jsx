c03e6fcfe700252b5850bb125441a5a5ab9ef8dc
Timztech <adetunjitimothy7@gmail.com>
2025-03-02 17:21:25 +0100

    MainContent.jsx corrected

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@
 const MainContent = () => {
   return (
     <main style={{ padding: "20px", textAlign: "center", backgroundColor: "#f4f4f4" }}>
-      <p style={{ fontSize: "18px" }}>Explore some of the most beautiful cities around the world!</p>
+      <p style={{ fontSize: "18px" }}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 };
