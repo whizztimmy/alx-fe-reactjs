2900d7a96172b40beb4365def6d68eeb8d9e8f76
Timztech <adetunjitimothy7@gmail.com>
2025-03-06 11:01:13 +0100

    navigate added to deleterecipebutton

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
 import useRecipeStore from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/'); // Redirect to the home page after deletion
   };
 
   return (
