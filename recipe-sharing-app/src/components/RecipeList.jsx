fa6971d6995bdf097cf452157f661b10f56eaa7c
Timztech <adetunjitimothy7@gmail.com>
2025-03-06 10:38:13 +0100

    zustand project 1 fixed

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from '../store/recipeStore';
+import useRecipeStore from './RecipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
