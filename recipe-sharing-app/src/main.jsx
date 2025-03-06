3ccffd4539956bd8063a2790e4b846ee212c9242
Timztech <adetunjitimothy7@gmail.com>
2025-03-06 11:31:50 +0100

    'fixed

--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
+import App from './App.jsx';
 import RecipeDetails from './components/RecipeDetails';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
