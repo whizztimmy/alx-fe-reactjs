79d2d211419f7b5c2c6e6799fb71b1ac429f88a7
Timztech <adetunjitimothy7@gmail.com>
2025-03-13 20:30:07 +0100

    Error Fixed

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PostsComponent from "./components/PostsComponent";
+
+// Create a QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <h1>React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query - Fetch Posts</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
