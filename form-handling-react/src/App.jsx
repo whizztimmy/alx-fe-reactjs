613ff5ffb24d6cb4cce4f237481dadd788ce07f5
Timztech <adetunjitimothy7@gmail.com>
2025-03-13 11:55:51 +0100

    fixed again

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,14 +1,14 @@
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <div>
       <h1>React Form Handling</h1>
-      <h2>Controlled Form</h2>
+      <h2>Controlled Form 1.0</h2>
       <RegistrationForm />
       <h2>Formik Form</h2>
-      <FormikForm />
+      <formikForm />
     </div>
   );
 }
