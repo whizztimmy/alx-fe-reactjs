fdc2a37ad4f690a11b5a85952673c4d6de8416d9
Timztech <adetunjitimothy7@gmail.com>
2025-03-16 16:18:24 +0100

    sm:p-4 added

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
@@ -17,5 +17,3 @@ function UserProfile() {
   }
   
   export default UserProfile;
-  
-  
