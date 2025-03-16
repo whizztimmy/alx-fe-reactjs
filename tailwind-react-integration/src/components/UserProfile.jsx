fa0ae0494781b37d69f8147e6753d21c4d622065
Timztech <adetunjitimothy7@gmail.com>
2025-03-16 16:22:53 +0100

    Responsive image sizing fixed

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
+          className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
         />
         <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-sm sm:text-base md:text-lg text-gray-600">
