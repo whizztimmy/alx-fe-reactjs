f83c7448fc41e10bad29db50ed5711c32a6ce1bc
Timztech <adetunjitimothy7@gmail.com>
2025-03-16 16:19:47 +0100

    all styling fixed

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
           className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
         />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-sm sm:text-base text-gray-600">
+        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
+        <p className="text-sm sm:text-base md:text-lg text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
@@ -17,3 +17,4 @@ function UserProfile() {
   }
   
   export default UserProfile;
+  
